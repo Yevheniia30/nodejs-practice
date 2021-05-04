55d6230cfc1252626a2e30ad99eea2d2d7af4d1a
Yevheniia Suska <susskaya86@gmail.com>
2021-05-05 02:15:37 +0300

    practice

---
+++ demo/path.js
@@ -0,0 +1,8 @@
+// import path from 'path'
+const path = require('path')
+
+console.log(path.basename(__filename));
+
+console.log(path.dirname(__filename));
+
+console.log(path.parse(__filename).name);
